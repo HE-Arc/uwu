adfc03c08c6d31c244e293ecf575251e42c8c039
ulysse.geinoz@he-arc.ch <ulysse.geinoz@he-arc.ch>
2022-03-14 12:50:07 +0100

    Added back to top button

--- uwuvue/src/App.vue
+++ uwuvue/src/App.vue
@@ -4,14 +4,18 @@
   <main class="container p-5 p">
     <router-view/>
   </main>
+
+  <top-button/>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import TopButton from '@/components/TopButton.vue'
 
 export default {
   components: {
     NavBar,
+    TopButton
   },
 
   created() {
---
+++ uwuvue/src/components/TopButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button v-if="visible" v-on:click="scrollToTop" class="btn btn-primary position-fixed bottom-0 end-0 m-2">back to top</button>
+</template>
+
+<script>
+export default {
+  name : 'TopButton',
+
+  data() {
+    return {
+      visible: false
+    }
+  },
+
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+
+  methods: {
+    handleScroll() {
+      this.visible = window.pageYOffset > window.innerHeight
+    },
+
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    }
+  }
+}
+</script>
